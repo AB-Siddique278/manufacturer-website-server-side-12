2e4b9913d447e406091ec4b8a87b5cb9d11dc46c
AB-Siddique278 <absiddique1060@gmail.com>
2022-05-23 03:07:44 +0600

    make a requar admin

--- index.js
+++ index.js
@@ -3,105 +3,141 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { ObjectID } = require('bson');
+const res = require('express/lib/response');
 require('dotenv').config();
 const app = express()
-const port =process.env.PORT||8000
- 
+const port = process.env.PORT || 8000
+
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ewthq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJWT(req, res, next){
-     const authorization =req.headers.authorization;
-     if(!authorization){
-         return res.status(401).send({message: 'UnAuthorization access'})
-     }
-     const token = authorization.split(' ')[1];
-     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
-         if(err){
-             return res.status(403).send({message: 'Forbiden access'})
-         }
-         req.decoded=decoded;
-         next();
-     });
+function verifyJWT(req, res, next) {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ message: 'UnAuthorization access' })
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbiden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
 }
 
 
 
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const serviceCollection = client.db('motor_parts').collection('services');
         const bookingCollection = client.db('motor_parts').collection('bookings');
         const userCollection = client.db('motor_parts').collection('users');
-        
+
         //for all service
-        app.get('/service', async(req, res)=>{
+        app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
+        //for admin chack if u atre admin u can make access
+        app.get('/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const user = await userCollection.findOne({email: email});
+            const isAdmin = user.role === 'admin';
+            res.send({admin: isAdmin})
+          })
+
+        //for Admin
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else{
+                res.status(403).send({message: 'forbiden'});
+            }
+
+        })
 
         // make admin and users
-        app.put('/user/:email', async(req, res)=>{
-            const email=req.params.email;
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
             const user = req.body;
-            const filter= {email:email};
-            const options = {upsert: true};
+            const filter = { email: email };
+            const options = { upsert: true };
             const updateDoc = {
-                $set:user,
+                $set: user,
 
             };
             const result = await userCollection.updateOne(filter, updateDoc, options)
-            const token = jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
-            
-            res.send({result,token});
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({ result, token });
         })
 
         // service for id
-        app.get('/service/:id', async(req, res)=>{
+        app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectID(id)};
+            const query = { _id: ObjectID(id) };
             const services = await serviceCollection.findOne(query);
             res.send(services);
         })
 
         //for dashbord bookin show 
 
-        app.get('/booking', verifyJWT, async(req, res)=>{
+        app.get('/booking', verifyJWT, async (req, res) => {
             const customer_email = req.query.customer_email
 
 
 
             const decodedEmail = req.decoded.email
-            if(customer_email===decodedEmail){
-                const query ={customer_email: customer_email};
+            if (customer_email === decodedEmail) {
+                const query = { customer_email: customer_email };
                 const bookings = await bookingCollection.find(query).toArray();
-               return res.send(bookings);
+                return res.send(bookings);
 
             }
-            else{
-                return res.status(403).send({message:'forbiden access'})
+            else {
+                return res.status(403).send({ message: 'forbiden access' })
             }
-         
+
 
         })
 
 
         //for booking service 
-        app.post('/booking', async(req, res)=>{
+        app.post('/booking', async (req, res) => {
             const booking = req.body;
             const result = await bookingCollection.insertOne(booking);
-            res.send({success: true, result});
+            res.send({ success: true, result });
+        });
+
+
+
+        //API for all users
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
         })
 
 
     }
-    finally{
+    finally {
 
     }
 }
@@ -110,9 +146,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
- 
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
