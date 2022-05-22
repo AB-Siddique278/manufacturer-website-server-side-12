9dc7f92785b65f62f34244e8ebf526e43986e169
AB-Siddique278 <absiddique1060@gmail.com>
2022-05-22 23:28:47 +0600

    make verifyJWT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { ObjectID } = require('bson');
 require('dotenv').config();
 const app = express()
@@ -12,18 +13,53 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ewthq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next){
+     const authorization =req.headers.authorization;
+     if(!authorization){
+         return res.status(401).send({message: 'UnAuthorization access'})
+     }
+     const token = authorization.split(' ')[1];
+     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+         if(err){
+             return res.status(403).send({message: 'Forbiden access'})
+         }
+         req.decoded=decoded;
+         next();
+     });
+}
+
+
+
+
 async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db('motor_parts').collection('services');
         const bookingCollection = client.db('motor_parts').collection('bookings');
-
+        const userCollection = client.db('motor_parts').collection('users');
+        
         //for all service
         app.get('/service', async(req, res)=>{
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        // make admin and users
+        app.put('/user/:email', async(req, res)=>{
+            const email=req.params.email;
+            const user = req.body;
+            const filter= {email:email};
+            const options = {upsert: true};
+            const updateDoc = {
+                $set:user,
+
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+            
+            res.send({result,token});
         })
 
         // service for id
@@ -34,6 +70,26 @@ async function run(){
             res.send(services);
         })
 
+        //for dashbord bookin show 
+
+        app.get('/booking', verifyJWT, async(req, res)=>{
+            const customer_email = req.query.customer_email
+
+
+
+            const decodedEmail = req.decoded.email
+            if(customer_email===decodedEmail){
+                const query ={customer_email: customer_email};
+                const bookings = await bookingCollection.find(query).toArray();
+               return res.send(bookings);
+
+            }
+            else{
+                return res.status(403).send({message:'forbiden access'})
+            }
+         
+
+        })
 
 
         //for booking service 
